c856c9e155b75dbc5da06f105a7b431ab5c2e3c2
Jacob Klimenko <droidedjake@gmail.com>
2021-07-31 23:31:36 -0400

    commit tree dea352564271c9e7585ead6bff3d3a8e341eeeed

--- index.js
+++ index.js
@@ -1,4 +1,63 @@
-/* Your Code Here */
+function createEmployeeRecord(employeeInfo) {
+    return {
+        firstName: employeeInfo[0],
+        familyName: employeeInfo[1],
+        title: employeeInfo[2],
+        payPerHour: employeeInfo[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+};
+
+function createEmployeeRecords(employeesInfo) {
+    return employeesInfo.map(e => createEmployeeRecord(e));
+};
+
+function createTimeInEvent(dateStamp) {
+    const timeInEvent = {
+        type: "TimeIn",
+        hour: parseInt(dateStamp.split(' ')[1]),
+        date: dateStamp.split(' ')[0]
+    };
+    this.timeInEvents.push(timeInEvent);
+    return this;
+};
+
+function createTimeOutEvent(dateStamp) {
+    const timeOutEvent = {
+        type: "TimeOut",
+        hour: parseInt(dateStamp.split(' ')[1]),
+        date: dateStamp.split(' ')[0]
+    };
+    this.timeOutEvents.push(timeOutEvent);
+    return this;
+};
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(e => e.date === date).hour;
+    const timeOut = this.timeOutEvents.find(e => e.date === date).hour;
+    return (timeOut - timeIn) / 100;
+};
+
+function wagesEarnedOnDate(date) {
+    const hours = hoursWorkedOnDate.call(this, date);
+    return hours * this.payPerHour;
+};
+
+function findEmployeeByFirstName(employeesRecords, firstName) {
+    return employeesRecords.find(e => e.firstName === firstName);
+};
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce(function(total, e) {
+        const dates = e.timeInEvents.map(e => e.date);
+        const wage = dates.reduce(function(total, date) {
+            return wagesEarnedOnDate.call(e, date) + total;
+        }, 0);
+
+        return wage + total;
+    }, 0);
+};
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
